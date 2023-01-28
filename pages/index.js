b2fd7a40ec4e2a1beef5c051f7be1b6d9fa7e3d2
w.benarto <w.benarto@gmail.com>
2023-01-28 22:13:13 +0200

    mouse hover

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,27 @@ const NoSsr = dynamic(() => import("../components/Landing"), {
 export default function Home() {
   // const [wh, setWh] = useState(100);
   // // Mouse tracking element
-  // const circleRefs = useRef([]);
-  // circleRefs.current = [];
+  const circleRefs = useRef([]);
+  circleRefs.current = [];
 
-  // useEffect(() => {
-  //   circleRefs.current.forEach((ref) =>
-  //     ref.moveTo(window.innerWidth / 2, window.innerHeight / 2)
-  //   );
+  useEffect(() => {
+    circleRefs.current.forEach((ref) =>
+      ref.moveTo(window.innerWidth / 2, window.innerHeight / 2)
+    );
 
-  //   const onMove = ({ clientX, clientY }) => {
-  //     circleRefs.current.forEach((ref) => ref.moveTo(clientX, clientY));
-  //   };
-  //   window.addEventListener("pointermove", onMove);
+    const onMove = ({ clientX, clientY }) => {
+      circleRefs.current.forEach((ref) => ref.moveTo(clientX, clientY));
+    };
+    window.addEventListener("pointermove", onMove);
 
-  //   return () => window.removeEventListener("pointermove", onMove);
-  // }, []);
+    return () => window.removeEventListener("pointermove", onMove);
+  }, []);
 
-  // const addCircleRef = (ref) => {
-  //   if (ref) {
-  //     circleRefs.current.push(ref);
-  //   }
-  // };
+  const addCircleRef = (ref) => {
+    if (ref) {
+      circleRefs.current.push(ref);
+    }
+  };
 
   // useEffect(() => {
   //   setWh(window.innerHeight * 0.01);
@@ -53,9 +53,9 @@ export default function Home() {
         >
           <NoSsr id="landing" />
         </div>
-        {/* <Circle size={12} ref={addCircleRef} delay={0} />
+        <Circle size={12} ref={addCircleRef} delay={0} />
         <Circle size={8} ref={addCircleRef} delay={0.1} />
-        <Circle size={4} ref={addCircleRef} delay={0.2} /> */}
+        <Circle size={4} ref={addCircleRef} delay={0.2} />
       </main>
 
       <footer></footer>
